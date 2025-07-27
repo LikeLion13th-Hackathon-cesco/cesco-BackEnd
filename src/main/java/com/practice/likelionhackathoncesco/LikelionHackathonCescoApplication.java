14425dd63c7bb042ad438382d1bed0f43a6cf83d
Juyong Park <imjuyongp@gmail.com>
2025-07-27 16:28:48 +0900

    :tada:Start: first commit

---
+++ src/main/java/com/practice/likelionhackathoncesco/LikelionHackathonCescoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.likelionhackathoncesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHackathonCescoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LikelionHackathonCescoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/practice/likelionhackathoncesco/LikelionHackathonCescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.likelionhackathoncesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionHackathonCescoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

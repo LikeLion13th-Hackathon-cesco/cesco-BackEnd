eb065c5d3fda0d8bb1048a365faf50e1a6c31d46
Juyong Park <imjuyongp@gmail.com>
2025-07-30 03:40:11 +0900

    :sparkles:Feat: 사용자 엔티티 설계

---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/user/entity/PayStatus.java
@@ -0,0 +1,11 @@
+package com.practice.likelionhackathoncesco.domain.user.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public enum PayStatus {
+  @Schema(description = "결제 완료")
+  PAID,
+
+  @Schema(description = "결제 미완료")
+  UNPAID;
+}

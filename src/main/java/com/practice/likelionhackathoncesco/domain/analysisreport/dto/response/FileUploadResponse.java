cf765857d43cb2f98cf28bbcc5881e205be49104
Juyong Park <imjuyongp@gmail.com>
2025-07-30 03:39:51 +0900

    :sparkles:Feat: 분석 레포트 엔티티 설계 + 파일업로드 서비스 로직

---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/dto/response/FileUploadResponse.java
@@ -0,0 +1,22 @@
+package com.practice.likelionhackathoncesco.domain.analysisreport.dto.response;
+
+import com.practice.likelionhackathoncesco.domain.analysisreport.entity.ProcessingStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FileUploadResponse {
+
+  // 파일 업로드를 하면 사용자에게 업로드한 파일 이름과 파일 업로드 성공 여부만 보여짐
+
+  @Schema(description = "업로드한 파일 이름", example = "등기부등본.pdf")
+  private String fileName;
+
+  private
+
+  @Schema(description = "진행 상태(파일 업로드 성공)")
+  private ProcessingStatus processingStatus;
+
+}
---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/repository/AnalysisReportRepository.java
@@ -0,0 +1,8 @@
+package com.practice.likelionhackathoncesco.domain.analysisreport.repository;
+
+import com.practice.likelionhackathoncesco.domain.analysisreport.entity.AnalysisReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalysisReportRepository extends JpaRepository<AnalysisReport, Long> {
+
+}

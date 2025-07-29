1cc8c83586a997d2b186df9c056b7f8712fbf79a
Juyong Park <imjuyongp@gmail.com>
2025-07-29 21:59:56 +0900

    :sparkles:Feat: 파일 초기 구조 설정 -> 환경변수는 직접 pull 받아야 함

---
+++ src/main/java/com/practice/likelionhackathoncesco/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.practice.likelionhackathoncesco.global.exception;
+
+
+import com.practice.likelionhackathoncesco.global.exception.model.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+  private final BaseErrorCode errorCode;
+
+  public CustomException(BaseErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}

baa5da4968c4f90f3ddfb7753ee99cfb85f07634
Juyong Park <imjuyongp@gmail.com>
2025-08-01 22:03:05 +0900

    :recycle:Refactor: reportId 응답 필드 추가

--- src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/dto/response/FileUploadResponse.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/dto/response/FileUploadResponse.java
@@ -11,6 +11,9 @@ public class FileUploadResponse {
 
   // 파일 업로드를 하면 사용자에게 업로드한 파일 이름과 파일 업로드 성공 여부만 보여짐
 
+  @Schema(description = "업로드한 파일 Id", example = "1")
+  private Long reportId;
+
   @Schema(description = "업로드한 파일 이름", example = "등기부등본.pdf")
   private String fileName;
 

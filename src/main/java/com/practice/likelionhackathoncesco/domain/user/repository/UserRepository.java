aac6eed83682d24bf223382e7c6bb39062ee56d6
Juyong Park <imjuyongp@gmail.com>
2025-08-02 04:36:25 +0900

    :wrench:Settings: submodule push

---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/user/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.practice.likelionhackathoncesco.domain.user.repository;
+
+public interface UserRepository {
+}
---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/user/service/UserPayService.java
@@ -0,0 +1,4 @@
+package com.practice.likelionhackathoncesco.domain.user.service;
+
+public class UserPayService {
+}

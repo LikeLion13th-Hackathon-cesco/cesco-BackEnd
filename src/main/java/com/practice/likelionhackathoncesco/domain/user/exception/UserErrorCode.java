a8fed8f1b2ea952c7f7bffcc0f67ab0083ed67f3
Juyong Park <imjuyongp@gmail.com>
2025-08-02 17:32:47 +0900

    sparkles:Feat: 사용자 관련 errorcode 추가

--- src/main/java/com/practice/likelionhackathoncesco/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/user/exception/UserErrorCode.java
@@ -1,4 +1,19 @@
 package com.practice.likelionhackathoncesco.domain.user.exception;
 
-public class UserErrorCode {
+import com.practice.likelionhackathoncesco.global.exception.model.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements BaseErrorCode {
+
+  USER_NOT_FOUND("USER_NOT_FOUND", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+  private final String code;
+  private final String message;
+  private final HttpStatus status;
+
 }

e0b5b81370ca57bc12a16b0610fe330be1276ce7
Juyong Park <imjuyongp@gmail.com>
2025-08-05 22:35:50 +0900

    :sparkles:Feat: ocr api invokeUrl, APIGW secretKey 정의

--- src/main/java/com/practice/likelionhackathoncesco/naverocr/global/config/NaverOcrConfig.java
+++ src/main/java/com/practice/likelionhackathoncesco/naverocr/global/config/NaverOcrConfig.java
@@ -1,8 +1,10 @@
-package com.practice.likelionhackathoncesco.naverocr.global;
+package com.practice.likelionhackathoncesco.naverocr.global.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Getter
 @Configuration
@@ -12,5 +14,11 @@ public class NaverOcrConfig {
   private String invokeUrl;
   @Value("${naver.ocr.secret-key}")
   private String secretKey;
+
+  // Spring Boot 2.4 이상에서는 RestTemplate이 자동으로 등록되지 않기 때문에 직접 Bean으로 등록해줘야 합니다.
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
 }

ea18f467628fbd5b5f52e1235fc9257389e782fb
Juyong Park <imjuyongp@gmail.com>
2025-08-06 02:22:50 +0900

    :recycle:Refactor: s3 다운로드 ErrorCode 추가

--- src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/exception/S3ErrorCode.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/exception/S3ErrorCode.java
@@ -14,7 +14,8 @@ public enum S3ErrorCode implements BaseErrorCode {
   FILE_SIZE_INVALID("PDF4002", "파일 크기는 50MB를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
   FILE_TYPE_INVALID("PDF4003", "PDF 파일만 업로드 가능합니다.", HttpStatus.BAD_REQUEST),
   FILE_SERVER_ERROR("PDF5001", "PDF파일 처리 중 서버 에러, 관리자에게 문의 바랍니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  INVALID_BASE64("PDF4005", "잘못된 Base64값 입니다.", HttpStatus.BAD_REQUEST);
+  INVALID_BASE64("PDF4005", "잘못된 Base64값 입니다.", HttpStatus.BAD_REQUEST),
+  FILE_DOWNLOAD_FAIL("PDF4006", "S3에서 다운로드를 실패했습니다.",HttpStatus.INTERNAL_SERVER_ERROR);
 
   private final String code;
   private final String message;

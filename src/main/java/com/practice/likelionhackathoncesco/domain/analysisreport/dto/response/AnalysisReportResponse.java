ef59bb5e7a8f7f858ec9ab80fa194785ce27d077
simGPT <2023216057@skuniv.ac.kr>
2025-08-12 03:32:23 +0900

    :wrench:Settings: submodule 수정

---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/dto/response/AnalysisReportResponse.java
@@ -0,0 +1,4 @@
+package com.practice.likelionhackathoncesco.domain.analysisreport.dto.response;
+
+public class AnalysisResultResponse {
+}
---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/analysisreport/service/AnalysisFlowService.java
@@ -0,0 +1,4 @@
+package com.practice.likelionhackathoncesco.domain.analysisreport.service;
+
+public class AnalysisFlowService {
+}

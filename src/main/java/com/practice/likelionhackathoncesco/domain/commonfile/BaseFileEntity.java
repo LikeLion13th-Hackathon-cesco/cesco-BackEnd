6bc19bb62c24e3c46c967d54bba94fe179ed0032
Juyong Park <imjuyongp@gmail.com>
2025-08-15 01:19:19 +0900

    :recycle:Refactor: BaseFileEntity 추상 엔티티에 user 필드 추가

--- src/main/java/com/practice/likelionhackathoncesco/domain/commonfile/BaseFileEntity.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/commonfile/BaseFileEntity.java
@@ -1,8 +1,12 @@
 package com.practice.likelionhackathoncesco.domain.commonfile;
 
+import com.practice.likelionhackathoncesco.domain.user.entity.User;
 import com.practice.likelionhackathoncesco.global.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +25,10 @@ public abstract class BaseFileEntity extends BaseTimeEntity {
 
   @Column(name = "s3_key", nullable = false)
   private String s3Key; // s3 객체 식별 키
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false) // user_id로 통일 (userId → user_id)
+  private User user;
 
   protected BaseFileEntity() {
     super(); // 부모 기본 생성자 호출
---
+++ src/main/java/com/practice/likelionhackathoncesco/domain/commonfile/service/FileService.java
@@ -0,0 +1,4 @@
+package com.practice.likelionhackathoncesco.domain.commonfile.service;
+
+public class fileService {
+}

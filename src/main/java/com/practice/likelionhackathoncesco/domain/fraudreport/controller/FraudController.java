944acf21d1cbfe6028ce6bf3880dddeaa3085f90
Juyong Park <imjuyongp@gmail.com>
2025-08-18 19:57:09 +0900

    :recycle:Refactor: 신고 관련문서 업로드 후 분석 api(로직 수정)

--- src/main/java/com/practice/likelionhackathoncesco/domain/fraudreport/controller/FraudController.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/fraudreport/controller/FraudController.java
@@ -1,12 +1,10 @@
 package com.practice.likelionhackathoncesco.domain.fraudreport.controller;
 
-import com.practice.likelionhackathoncesco.domain.analysisreport.entity.PathName;
-import com.practice.likelionhackathoncesco.domain.fraudreport.dto.response.ComplaintResponse;
 import com.practice.likelionhackathoncesco.domain.fraudreport.dto.response.FakerResponse;
-import com.practice.likelionhackathoncesco.domain.fraudreport.dto.response.FraudRegisterResponse;
+import com.practice.likelionhackathoncesco.domain.fraudreport.dto.response.FraudResponse;
 import com.practice.likelionhackathoncesco.domain.fraudreport.service.ComplaintReportUpload;
 import com.practice.likelionhackathoncesco.domain.fraudreport.service.FakerSaveFlow;
-import com.practice.likelionhackathoncesco.domain.fraudreport.service.FraudRegisterReportUpload;
+import com.practice.likelionhackathoncesco.domain.fraudreport.service.FraudReportUpload;
 import com.practice.likelionhackathoncesco.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -16,8 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,63 +27,30 @@ import org.springframework.web.multipart.MultipartFile;
 @Tag(name = "Fraud Report Upload", description = "신고 관련 문서 업로드 관련 API")
 public class FraudController {
 
-  private final FraudRegisterReportUpload fraudRegisterReportUpload;
+  private final FraudReportUpload fraudReportUpload;
   private final ComplaintReportUpload complaintReportUpload;
   private final FakerSaveFlow fakerSaveFlow;
 
-  // 신고 등기부등본 업로드 후 해당 임대인 정보 저장 API
   @Operation(
-      summary = "신고 등기부등본 업로드 후 해당 임대인 정보 저장 API",
-      description = "신고할 등기부등본 업로드 된 후 바로 신고 임대인 정보 저장")
-  @PostMapping(value = "/save/{reportId}")
+      summary = "신고 관련 문서 모두 업로드 후 임대인 정보 저장 API",
+      description = "신고 관련 문서 모두 업로드 후 제출하기 버튼을 클릭하면 업로드 상태를 리턴하는 API")
+  @PostMapping(value = "/reports", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<BaseResponse<List<FakerResponse>>> saveFakerInfo(
-      @Parameter(description = "신고 등기부등본 고유번호") @PathVariable Long reportId) {
-    List<FakerResponse> fakerResponseList = fakerSaveFlow.processSaveFakerInfo(reportId);
+      @Parameter(description = "고소장 업로드") @RequestParam("complaint file")
+          MultipartFile complaintFile,
+      @Parameter(description = "신고할 등기부등본 업로드") @RequestParam("fraud report file")
+          MultipartFile fraudReportFile) {
+
+    // s3 업로드 후 DB 저장
+    FraudResponse fraudResponse =
+        fraudReportUpload.uploadFraudDocuments(complaintFile, fraudReportFile);
+
+    // 신고 등기부등본 id 조회
+    Long fraudReportId = fraudResponse.getFraudReportId();
+
+    // 사기꾼 파싱 후 faker DB에 저장
+    List<FakerResponse> fakerResponseList = fakerSaveFlow.processSaveFakerInfo(fraudReportId);
+
     return ResponseEntity.ok(BaseResponse.success("신고당한 임대인 정보 저장 완료", fakerResponseList));
-  }
-
-  @Operation(
-      summary = "신고 등기부등본 업로드 API",
-      description = "신고할 등기부등본 문서를 업로드하고 문서 원본이름과 상태를 리턴하는 API")
-  @PostMapping(value = "/documents", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<BaseResponse<FraudRegisterResponse>> uploadFraudFile(
-      @RequestParam MultipartFile file) {
-    FraudRegisterResponse fraudRegisterResponse =
-        fraudRegisterReportUpload.uploadDocuments(PathName.FRAUDREPORT, file);
-
-    return ResponseEntity.ok(BaseResponse.success("신고할 등기부등본 업로드 완료.", fraudRegisterResponse));
-  }
-
-  @Operation(summary = "신고 등기부등본 삭제 API", description = "X버튼을 눌러 업로드한 신고 등기부등본 문서를 삭제하는 API")
-  @DeleteMapping(value = "/documents/{reportId}")
-  public ResponseEntity<BaseResponse<Boolean>> deleteFraudRegisterReport(
-      @PathVariable Long reportId) {
-
-    log.info("파일 삭제 요청: reportId={}", reportId);
-
-    Boolean result = fraudRegisterReportUpload.deleteReport(reportId);
-
-    return ResponseEntity.ok(BaseResponse.success("파일이 삭제되었습니다.", result));
-  }
-
-  @Operation(summary = "고소장 업로드 API", description = "고소장 문서를 업로드하고 문서 원본이름과 상태를 리턴하는 API")
-  @PostMapping(value = "/complaints", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<BaseResponse<ComplaintResponse>> uploadComplaintFile(
-      @RequestParam MultipartFile file) {
-    ComplaintResponse complaintResponse =
-        complaintReportUpload.uploadDocuments(PathName.COMPLAINT, file);
-
-    return ResponseEntity.ok(BaseResponse.success("고소장 업로드 완료.", complaintResponse));
-  }
-
-  @Operation(summary = "고소장 삭제 API", description = "X버튼을 눌러 업로드한 고소장 문서를 삭제하는 API")
-  @DeleteMapping(value = "/complaints/{reportId}")
-  public ResponseEntity<BaseResponse<Boolean>> deleteComplaintReport(@PathVariable Long reportId) {
-
-    log.info("파일 삭제 요청: reportId={}", reportId);
-
-    Boolean result = complaintReportUpload.deleteReport(reportId);
-
-    return ResponseEntity.ok(BaseResponse.success("파일이 삭제되었습니다.", result));
   }
 }
--- src/main/java/com/practice/likelionhackathoncesco/domain/fraudreport/dto/request/FraudRequest.java
+++ src/main/java/com/practice/likelionhackathoncesco/domain/fraudreport/dto/request/FraudRequest.java
@@ -1,4 +1,25 @@
 package com.practice.likelionhackathoncesco.domain.fraudreport.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
 public class FraudRequest {
+
+  @Schema(
+      description = "업로드된 고소장 파일 (PDF 형식)",
+      type = "string",
+      format = "binary",
+      example = "파일을 선택하세요")
+  private MultipartFile complaintFile;
+
+  @Schema(
+      description = "업로드된 신고할 등기부등본 파일 (PDF 형식)",
+      type = "string",
+      format = "binary",
+      example = "파일을 선택하세요")
+  private MultipartFile fraudReportFile;
 }
